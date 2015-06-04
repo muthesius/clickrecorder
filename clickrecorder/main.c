248f5ff1c5b7e7c5188eaf52a9fb0158409b4a39
jens <jens@lea.io>
2015-06-04 08:18:52 +0200

    Initial Commit

---
+++ clickrecorder/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  clickrecorder
+//
+//  Created by jens on 04.06.15.
+//  Copyright (c) 2015 lea.io. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
